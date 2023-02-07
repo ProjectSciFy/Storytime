98c29123ac27acb9b63388cd98d19928c8e68a33
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2023-02-07 15:56:44 -0500

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+import dropdown
 pygame.init()
 pygame.font.init()
 
@@ -13,7 +15,11 @@ WINDOW_WIDTH = 1100
 WINDOW_HEIGHT = 700
 RESOLUTION = (WINDOW_WIDTH, WINDOW_HEIGHT)
 SCREEN = pygame.display.set_mode(RESOLUTION)
-FONT = pygame.font.SysFont('Arial',35)
+FONT_SIZE = 30
+FONT_SIZE_SMALL = 24
+FONT = pygame.font.SysFont('Verdana',FONT_SIZE)
+SMALL_FONT = pygame.font.SysFont('Verdana',FONT_SIZE_SMALL)
+BUTTON_EXPAND_BUFFER = 5
 # COLOR SCHEME:
 # Color order:
 #   colorscheme[0]-Main background:              Light
@@ -74,7 +80,7 @@ def displaySchemes():
         # IMPORTMANT NUMBERS:
         x_offset = 20
         y_offset = 80
-        locx = 280
+        locx = 320
         locy = -60
         size = 50
         offset = 5
@@ -82,7 +88,7 @@ def displaySchemes():
         # Print squares of each color scheme
         for scheme in colorschemes:
             text_button = FONT.render(f"Color Scheme #{schemNum}:" , True, "#ffffff")
-            locx = 280
+            locx = 320
             locy += y_offset
             SCREEN.blit(text_button, (x_offset + offset, locy + offset))
             for color in scheme:
@@ -125,56 +131,109 @@ def checkHover(pyButton: pygame.Surface, pyMouse: pygame.mouse, locx: int, locy:
     return hovering
 
 def test():
-    width = SCREEN.get_width()
-    height = SCREEN.get_height()
     # Grab colors from color scheme
     button_light = COLORSCHEME[Scheme("BUTTON_HOVER")]
     button_dark = COLORSCHEME[Scheme("BUTTON_NO_HOVER")]
     # Render colors, text, and font for quit button (hover/no hover) and main menu text message
     text_button_dark = FONT.render('Quit' , True , COLORSCHEME[Scheme("TEXT_NO_HOVER")])
     text_button_light = FONT.render('Quit' , True , COLORSCHEME[Scheme("TEXT_HOVER")])
+    FONT.set_bold(True)
+    FONT.set_underline(True)
     text_main_menu = FONT.render('Welcome to Storytime!' , True , COLORSCHEME[Scheme("TEXT_NO_HOVER")])
+    text_select_menu = FONT.render('Customize Story:' , True , COLORSCHEME[Scheme("TEXT_NO_HOVER")])
+    FONT.set_bold(False)
+    FONT.set_underline(False)
+    # Dropdowns setup
+    numDropdowns = 3
+    dropdownX = 30
+    dropdownWidth = 300
+    dropdownHeight = 50
+    dropdownY = list()
+    dropdownBuffer = 50
+    for i in range(numDropdowns):
+        dropdownY.append(dropdownHeight*i + dropdownBuffer*(i + 2))
+    genreDropdown = dropdown.DropDown(
+        [button_dark, button_light],
+        [COLORSCHEME[Scheme("BUTTON_NO_HOVER")], COLORSCHEME[Scheme("BUTTON_HOVER")]],
+        COLORSCHEME[Scheme("TEXT_NO_HOVER")],
+        dropdownX, dropdownY[0], dropdownWidth, dropdownHeight, 
+        pygame.font.SysFont('Verdana', FONT_SIZE_SMALL), 
+        "Select Genre:", ["Fiction", "History"])
+    lengthDropdown = dropdown.DropDown(
+        [button_dark, button_light],
+        [COLORSCHEME[Scheme("BUTTON_NO_HOVER")], COLORSCHEME[Scheme("BUTTON_HOVER")]],
+        COLORSCHEME[Scheme("TEXT_NO_HOVER")],
+        dropdownX, dropdownY[1], dropdownWidth, dropdownHeight, 
+        pygame.font.SysFont('Verdana', FONT_SIZE_SMALL), 
+        "Select Length:", ["Short", "Medium", "Long"])
+    languageDropdown = dropdown.DropDown(
+        [button_dark, button_light],
+        [COLORSCHEME[Scheme("BUTTON_NO_HOVER")], COLORSCHEME[Scheme("BUTTON_HOVER")]],
+        COLORSCHEME[Scheme("TEXT_NO_HOVER")],
+        dropdownX, dropdownY[2], dropdownWidth, dropdownHeight, 
+        pygame.font.SysFont('Verdana', FONT_SIZE_SMALL), 
+        "Select Language:", ["English", "Spanish"])
 
     while True:
         # Create quit button text, outline, filling, and hovering variables
+        sideBarLWidth = 360
         button_h = text_button_dark.get_height()
         button_w = text_button_dark.get_width()
         offset_h = text_button_dark.get_height()//5
         offset_w = text_button_dark.get_width()//2
-        midButtonW = width/2 - button_w/2
-        midButtonH = height/2 - button_h/2
+        midButtonW = WINDOW_WIDTH/2 - button_w/2 + sideBarLWidth/2
+        midButtonH = WINDOW_HEIGHT/2 - button_h/2
         buttonLocX = midButtonW - offset_w/2
         buttonLocY = midButtonH - offset_h/2
         buttonWithOffsetW = button_w + offset_w
         buttonWithOffsetH = button_h + offset_h
         # Rectangle for quit button
-        quitButtonRect = [buttonLocX, buttonLocY, buttonWithOffsetW, buttonWithOffsetH]
+        quitButtonRect = pygame.Rect(buttonLocX, buttonLocY, buttonWithOffsetW, buttonWithOffsetH)
         
         # Fill screen background and get mouse position
         SCREEN.fill(COLORSCHEME[Scheme("BACKGROUND")])
         mouse = pygame.mouse.get_pos()
         
         # Quit button interior (hover or no hover)
-        if checkHover(text_button_dark, mouse, width, height, offset_w, offset_h):
+        if checkHover(text_button_dark, mouse, WINDOW_WIDTH + sideBarLWidth, WINDOW_HEIGHT, offset_w, offset_h):
             pygame.draw.rect(SCREEN, button_light, quitButtonRect, border_radius=3)
-            SCREEN.blit(text_button_light, (midButtonW, midButtonH))
+            SCREEN.blit(text_button_light, text_button_light.get_rect(center = quitButtonRect.center))
         else:
             pygame.draw.rect(SCREEN, button_dark, quitButtonRect, border_radius=3)
-            SCREEN.blit(text_button_dark, (midButtonW, midButtonH))
+            SCREEN.blit(text_button_dark, text_button_dark.get_rect(center = quitButtonRect.center))
             
         # Quit button outline
         pygame.draw.rect(SCREEN, COLORSCHEME[6], quitButtonRect, 2, 3)
         
-        
         # Main menu text message
-        mainMenuTextLocX = width/2 - text_main_menu.get_width()/2
-        mainMenuTextLocY = height/2 - text_main_menu.get_height()/2-100
+        mainMenuTextLocX = WINDOW_WIDTH/2 - text_main_menu.get_width()/2 + sideBarLWidth/2
+        mainMenuTextLocY = WINDOW_HEIGHT/2 - text_main_menu.get_height()/2-100
         SCREEN.blit(text_main_menu, (mainMenuTextLocX, mainMenuTextLocY))
         
+        # Events:
+        event_list = pygame.event.get()
+        
+        # Update dropdowns
+        selected_option_genre = genreDropdown.update(event_list)
+        selected_option_length = lengthDropdown.update(event_list)
+        selected_option_language = languageDropdown.update(event_list)
+        if selected_option_genre >= 0:
+            genreDropdown.main = genreDropdown.options[selected_option_genre]
+        elif selected_option_length >= 0:
+            lengthDropdown.main = lengthDropdown.options[selected_option_length]
+        elif selected_option_language >= 0:
+            languageDropdown.main = languageDropdown.options[selected_option_language]
+        # Draw vertical dropdown separator
+        SCREEN.blit(text_select_menu, (dropdownX, dropdownHeight/2))
+        pygame.draw.line(SCREEN, COLORSCHEME[Scheme("TEXT_NO_HOVER")], (sideBarLWidth, 10), (sideBarLWidth, 690), width=2)
+        genreDropdown.draw(SCREEN)
+        lengthDropdown.draw(SCREEN)
+        languageDropdown.draw(SCREEN)
+        
         # Check for any button pushes or program closure
-        for event in pygame.event.get():
+        for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if checkHover(text_button_dark, mouse, SCREEN.get_width(), SCREEN.get_height()):
+                if checkHover(text_button_dark, mouse, SCREEN.get_width(), SCREEN.get_height(), offx=sideBarLWidth):
                     pygame.quit()
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -185,8 +244,8 @@ def test():
         
 #If you uncomment this line, you must comment out all lines below this function (they will be unreachable.)
 
-#Un-comment the line below to see all color schemes in "Colorschemes.txt".
-displaySchemes()
+# Un-comment the line below to see all color schemes in "Colorschemes.txt".
+# displaySchemes()
 
 ''''''''''''''''''''''''''''''''''''
 
@@ -194,5 +253,5 @@ displaySchemes()
 #You will need both line 152 and 153 un-commented in order to test the color schemes in "Colorschemes.txt".
 
 #Un-comment the line below to change color schemes
-# COLORSCHEME = applyColorScheme(1)
-# test()
+COLORSCHEME = applyColorScheme(5)
+test()
