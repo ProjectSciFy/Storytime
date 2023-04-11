63d6e0b090951d334d096137b2f64b92eec2868b
Gal <57294938+ProjectSciFy@users.noreply.github.com>
2023-04-11 16:18:32 -0400

    Update UI.py

--- UI.py
+++ UI.py
@@ -2,9 +2,20 @@ import pygame
 import sounds
 import utility as utl
 import helpers as h
+import json
 pygame.init()
 utl.MAIN_COLOR_SCHEME = 1
 utl.colorscheme = h.applyColorScheme(utl.MAIN_COLOR_SCHEME)   
+
+with open('rasa/rasa_pass.json','r+') as f:
+    f.seek(0)
+    json.dump({
+    "scheme": "Original",
+    "font": "Monaco",
+    "sound": "ENABLE",
+    "keywords": []
+}, f, indent=4)
+    f.truncate()
 
 def run(): 
     import main
@@ -15,6 +26,19 @@ def run():
     while running:
         mouse = pygame.mouse.get_pos()
         event_list = pygame.event.get()
+
+        with open('rasa/rasa_pass.json','r') as f:
+            data = json.load(f)
+            utl.Scheme = data['scheme']
+            utl.Font = data['font']
+            utl.Sound = data['sound']
+            
+        h.updateScheme(utl.Scheme)
+        utl.colorscheme = h.applyColorScheme(utl.MAIN_COLOR_SCHEME)   
+        h.updateFont(utl.Font)
+        h.updateSound(utl.Sound)
+
+
         if in_main_window:
             viewStoryButtonRect = main.updateMain(event_list)
         else:
@@ -23,7 +47,7 @@ def run():
         # Check for any button pushes or program closure
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if in_main_window and viewStoryButtonRect.collidepoint(mouse):
+                if in_main_window and viewStoryButtonRect.collidepoint(mouse) and len(utl.storyText) > 0:
                     if utl.Sound.upper() == "ENABLE":
                         sounds.playSound("success")
                     in_main_window = False
